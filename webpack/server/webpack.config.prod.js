7f136190d3da73b6e4272450a6792c7cd5857afb
Shun Eto [ prog ] <46951119+shun-Eto@users.noreply.github.com>
2021-01-28 14:37:08 +0900

    Delete webpack/server directory

--- webpack/server/webpack.config.prod.js
+++
@@ -1,8 +0,0 @@
-const baseConfig = require("./webpack.config.base.js");
-const merge = require("webpack-merge");
-
-const config = merge(baseConfig, {
-    mode: "production"
-});
-
-module.exports = config;
