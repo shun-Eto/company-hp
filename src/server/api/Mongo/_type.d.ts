4a6e106c876e6ff2f5971894c9d3877dbfbbf342
Shun Eto [ prog ] <46951119+shun-Eto@users.noreply.github.com>
2021-01-28 14:37:26 +0900

    Delete src/server directory

--- src/server/api/Mongo/_type.d.ts
+++
@@ -1,7 +0,0 @@
-import MongoDB from "mongodb";
-
-//	types
-export type Filter = MongoDB.FilterQuery<any>;
-export type FindOneOptions = MongoDB.FindOneOptions;
-export type Update = MongoDB.UpdateQuery<any> | Partial<any>;
-export type UpdateOptions = MongoDB.UpdateManyOptions;
